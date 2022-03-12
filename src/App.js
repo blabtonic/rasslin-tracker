89e2efdf213ec6c2ae86d5743b59c2a33371be0f
Trystan Hendricks <dolkhound@gmx.com>
2022-03-12 09:11:17 -0500

    Box added and centered still in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import Button from '@mui/material/Button';
-import { Container, createTheme, ThemeProvider } from '@mui/material';
-import { lime } from '@mui/material/colors';
+import {
+  Box,
+  Container,
+  createTheme,
+  Grid,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: lime['A400'],
+      main: blue['A100'],
     },
   },
 });
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <ThemeProvider theme={theme}>
-        <Button variant="contained">Hello World</Button>
-      </ThemeProvider>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            bgcolor: '#696969',
+          }}
+        >
+          <Typography component="h2" variant="h5">
+            Rasslin Tracker
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="firstWrestler"
+                  id="first-Wrestler"
+                  fullWidth
+                  label="Wrestler #1"
+                  autoFocus
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Button variant="contained">Track</Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
