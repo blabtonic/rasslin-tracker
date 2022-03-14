e416f85d93f60d8923a50f198ce42cc72ca59978
Trystan Hendricks <dolkhound@gmx.com>
2022-03-14 00:50:23 -0400

    star precision added

--- src/Components/StarRatings.js
+++ src/Components/StarRatings.js
@@ -3,14 +3,19 @@ import { Box, Rating, Typography } from '@mui/material';
 import StarIcon from '@mui/icons-material/Star';
 
 const labels = {
+  0.25: 'Trash',
   0.5: 'Awful',
   1: 'Bad',
+  1.25: 'Terrible',
   1.5: 'Wretched',
   2: 'Ok',
-  2.5: 'Ok',
-  3: 'Average',
-  3.5: 'Average',
+  2.25: 'Middling',
+  2.5: 'Average',
+  3: 'Watchable',
+  3.25: 'Entertaining',
+  3.5: 'Good',
   4: 'Great',
+  4.25: 'Really Great',
   4.5: 'A Must watch',
   5: 'Amazing',
 };
@@ -24,7 +29,7 @@ export default function StarRatings() {
       <Rating
         name="rating-feedback"
         value={0}
-        precision={0.5}
+        precision={0.25}
         onChange={(event, newValue) => {
           setValue(newValue);
         }}
